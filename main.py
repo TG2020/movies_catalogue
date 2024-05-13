42a7285a46e3f116f9b24bbb7055612db158565b
Tomasz Graczyk <tgraczyk86@gmail.com>
2024-05-13 17:34:20 +0200

    movies_catalogue

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def homepage():
     return render_template("index.html")
 
 
-@app.route('/')
-def homepage():
+@app.route('/<int:movie_id>')
+def page_one(movie_id):
     movies = []
     return render_template("homepage.html", movies=movies)
 
