c874bed37f025f2afcabe4052010a0d3645ff14a
Tomasz Graczyk <tgraczyk86@gmail.com>
2024-06-02 11:54:33 +0200

    movies_catalogue

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def homepage():
-    return render_template("index.html")
-
-
-@app.route('/<int:movie_id>')
-def page_one(movie_id):
-    movies = []
-    return render_template("homepage.html", movies=movies)
-
+    movies = [{"title": "Film 1", "description": "Opis filmu 1", "image_url": "link_do_obrazka1.jpg"},
+    {"title": "Film 2", "description": "Opis filmu 2", "image_url": "link_do_obrazka2.jpg"},
+    {"title": "Film 3", "description": "Opis filmu 3", "image_url": "link_do_obrazka3.jpg"},
+]
+    return render_template('homepage.html', movies=movies)
 
 if __name__ == '__main__':
     app.run(debug=True)
